773a003e59f0a9c482a78ecddf78dca1fbfef171
Can Alkan <calkan@gmail.com>
2018-10-30 18:59:54 +0300

    added test_tardis_docker.sh

---
+++ test_tardis_docker.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This script acts as a demonstration of TARDIS usage in a simple case
+# It downloads a single-chromosome BAM file from the 1000 Genomes Project, and associated reference genome and SONIC files
+# Downloading these files will take a while. If they are pre-downloaded, just comment out the WGET lines below
+# It then runs TARDIS on this data set. Since it is a single-chrmosome file, TARDIS will print information that shows
+# no data is found in all other chromosomes
+# This script also assumes TARDIS is downloaded, compiled, and available in the PATH.
+# Prerequsites:
+#	TARDIS
+#	wget
+# 	gzip
+# 	samtools
+
+WGET_EXE=`which wget`
+SAMTOOLS_EXE=`which samtools`
+
+# Download NA11930 chromosome 20 BAM
+${WGET_EXE} -c ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/phase3/data/NA11930/alignment/NA11930.chrom20.ILLUMINA.bwa.CEU.low_coverage.20130415.bam
+# Download NA11930 chromosome 20 BAI
+${WGET_EXE} -c ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/phase3/data/NA11930/alignment/NA11930.chrom20.ILLUMINA.bwa.CEU.low_coverage.20130415.bam.bai
+
+# Download reference
+${WGET_EXE} -c ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/human_g1k_v37.fasta.gz
+${WGET_EXE} -c ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/human_g1k_v37.fasta.fai
+
+# unzip reference
+
+gunzip human_g1k_v37.fasta.gz
+if [ $? -eq 0 ] 
+then
+	echo "Reference genome seems to be downloaded correctly.";
+else
+	echo "Reference genome seems NOT to be downloaded correctly. Please rerun this script.";
+	exit 1;
+fi
+
+
+# Download SONIC file for the same reference
+${WGET_EXE} -c https://github.com/BilkentCompGen/sonic-prebuilt/raw/master/human_g1k_v37.sonic
+
+# these are the parameters. Commend out the WGET lines aboove if they are predownloaded
+INPUTBAM=NA11930.chrom20.ILLUMINA.bwa.CEU.low_coverage.20130415.bam
+REF=human_g1k_v37.fasta
+SONIC=human_g1k_v37.sonic
+
+${SAMTOOLS_EXE} quickcheck $INPUTBAM
+if [ $? -eq 0 ] 
+then
+	echo $INPUTBAM " seems to be downloaded correctly according to samtools quickcheck";
+else
+	echo $INPUTBAM " seems NOT to be downloaded correctly according to samtools quickcheck. Please rerun this script.";
+	exit 1;
+fi
+
+# pull docker image. Comment this out if it is already pulled
+docker pull alkanlab/tardis
+
+# ready to call TARDIS 
+docker run --user=$UID -w ${PWD} -v ${PWD}:/input -v ${PWD}:/output alkanlab/tardis  -i /input/$INPUTBAM --ref /input/$REF --sonic /input/$SONIC --out /output/NA11930-chrom20-demo
